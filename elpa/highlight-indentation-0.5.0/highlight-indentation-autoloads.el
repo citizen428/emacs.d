b1b7866563bb81d5a43f00de9c408a97056865bd
Michael Kohl <citizen428@gmail.com>
2011-09-01 21:19:24 +0200

    highlight-indentation mode

---
+++ elpa/highlight-indentation-0.5.0/highlight-indentation-autoloads.el
@@ -0,0 +1,18 @@
+;;; highlight-indentation-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("highlight-indentation-pkg.el" "highlight-indentation.el")
+;;;;;;  (20063 32946 271123))
+
+;;;***
+
+(provide 'highlight-indentation-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; highlight-indentation-autoloads.el ends here
