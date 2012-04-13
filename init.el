82e8d700770b2d1e0596742c841652bbc8b9d6be
Michael Kohl <citizen428@gmail.com>
2012-04-13 21:15:03 +0200

    More cleanup

--- init.el
+++ init.el
@@ -55,3 +55,10 @@
 (load-local-file "config/org-mode-conf.el")
 (load-local-file "config/twittering-conf.el")
 (put 'ido-exit-minibuffer 'disabled nil)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
