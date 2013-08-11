f2473b840b139ab76d1a4eb19a84a8a960373bd4
Michael Kohl <citizen428@gmail.com>
2013-08-11 16:25:07 +0700

    There was more stuff to move to ELPA

---
+++ elpa/ac-slime-0.3/ac-slime-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ac-slime" "0.3" "An auto-complete source using slime completions" (quote nil))
--- elpa/ac-slime-0.3/ac-slime.el
+++ elpa/ac-slime-0.3/ac-slime.el
@@ -1,26 +1,36 @@
-;;----------------------------------------------------------------------------
-;; An auto-complete source using slime completions
+
+;;; ac-slime.el --- An auto-complete source using slime completions
 ;;
-;; Author: Steve Purcell <steve at sanityinc dot com>
+;;; Author: Steve Purcell <steve@sanityinc.com>
+;;; URL: https://github.com/purcell/ac-slime
+;;; Version: 0.3
 ;;
+;;; Commentary:
 ;; Usage:
-;;   (require 'ac-slime)
-;;   (add-hook 'slime-mode-hook 'set-up-slime-ac)
-;;   (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
-;;----------------------------------------------------------------------------
+;;     (require 'ac-slime)
+;;     (add-hook 'slime-mode-hook 'set-up-slime-ac)
+;;     (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+;;     (eval-after-load "auto-complete"
+;;       '(add-to-list 'ac-modes 'slime-repl-mode))
+;;
 
 (eval-when-compile (require 'cl))
 
 (defun ac-source-slime-fuzzy-candidates ()
   "Return a possibly-empty list of fuzzy completions for the symbol at point."
-  (if (slime-connected-p)
-      (let ((slime-fuzzy-completion-limit 50))
-        (mapcar 'car (car (slime-fuzzy-completions  ac-prefix))))))
+  (when (slime-connected-p)
+    (let ((slime-fuzzy-completion-limit 50))
+      (mapcar 'car (car (slime-fuzzy-completions (substring-no-properties ac-prefix)))))))
 
 (defun ac-source-slime-simple-candidates ()
   "Return a possibly-empty list of completions for the symbol at point."
-  (if (slime-connected-p)
-      (car (slime-simple-completions  ac-prefix))))
+  (when (slime-connected-p)
+    (car (slime-simple-completions (substring-no-properties ac-prefix)))))
+
+(defun ac-source-slime-case-correcting-completions (name collection)
+  (mapcar #'(lambda (completion)
+              (replace completion name))
+          (all-completions (downcase name) collection)))
 
 (defvar ac-slime-current-doc nil "Holds slime docstring for current symbol")
 (defun ac-slime-documentation (symbol-name)
@@ -30,16 +40,19 @@
 (defun ac-slime-init ()
   (setq ac-slime-current-doc nil))
 
+;;;###autoload
 (defface ac-slime-menu-face
-  '((t (:inherit 'ac-candidate-face)))
+  '((t (:inherit ac-candidate-face)))
   "Face for slime candidate menu."
   :group 'auto-complete)
 
+;;;###autoload
 (defface ac-slime-selection-face
-  '((t (:inherit 'ac-selection-face)))
+  '((t (:inherit ac-selection-face)))
   "Face for the slime selected candidate."
   :group 'auto-complete)
 
+;;;###autoload
 (defvar ac-source-slime-fuzzy
   '((init . ac-slime-init)
     (candidates . ac-source-slime-fuzzy-candidates)
@@ -47,9 +60,11 @@
     (selection-face . ac-slime-selection-face)
     (prefix . slime-symbol-start-pos)
     (symbol . "l")
+    (match . (lambda (prefix candidates) candidates))
     (document . ac-slime-documentation))
   "Source for fuzzy slime completion")
 
+;;;###autoload
 (defvar ac-source-slime-simple
   '((init . ac-slime-init)
     (candidates . ac-source-slime-simple-candidates)
@@ -57,18 +72,21 @@
     (selection-face . ac-slime-selection-face)
     (prefix . slime-symbol-start-pos)
     (symbol . "l")
-    (document . ac-slime-documentation))
+    (document . ac-slime-documentation)
+    (match . ac-source-slime-case-correcting-completions))
   "Source for slime completion")
 
 
+;;;###autoload
 (defun set-up-slime-ac (&optional fuzzy)
   "Add an optionally-fuzzy slime completion source to the
 front of `ac-sources' for the current buffer."
   (interactive)
-  (setq ac-sources (add-to-list 'ac-sources
-                                (if fuzzy
-                                    'ac-source-slime-fuzzy
-                                  'ac-source-slime-simple))))
+  (add-to-list 'ac-sources
+               (if fuzzy
+                   'ac-source-slime-fuzzy
+                 'ac-source-slime-simple)))
 
 
 (provide 'ac-slime)
+;;; ac-slime.el ends here
