eff86fbc7f55b083c9d1234e9acfc7348b24aba1
Michael Kohl <citizen428@gmail.com>
2013-09-12 22:05:37 +0700

    Add MELPA and switch to newer elixir-mode

--- elpa/elixir-mode-1.0.0/elixir-mode-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; elixir-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "elixir-mode" "elixir-mode.el" (20976 1218
-;;;;;;  861960 140000))
-;;; Generated autoloads from elixir-mode.el
-
-(add-to-list 'auto-mode-alist '("\\.ex\\'" . elixir-mode))
-
-(add-to-list 'auto-mode-alist '("\\.exs\\'" . elixir-mode))
-
-;;;***
-
-;;;### (autoloads nil nil ("elixir-mode-pkg.el") (20976 1219 9547
-;;;;;;  333000))
-
-;;;***
-
-(provide 'elixir-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; elixir-mode-autoloads.el ends here
---
+++ elpa/elixir-mode-20130721.2352/elixir-mode-pkg.el
@@ -0,0 +1,2 @@
+(define-package "elixir-mode" "20130721.2352" "Major mode for editing Elixir files [github]" 'nil)
+
