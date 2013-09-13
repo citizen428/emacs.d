6922eaa680f5a75e5d965c422212d63c4195bba3
Michael Kohl <citizen428@gmail.com>
2013-09-13 17:23:12 +0700

    Update buffer-move

--- elpa/buffer-move-0.4/buffer-move-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; buffer-move-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("buffer-move-pkg.el" "buffer-move.el")
-;;;;;;  (19891 3880 343952))
-
-;;;***
-
-(provide 'buffer-move-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; buffer-move-autoloads.el ends here
---
+++ elpa/buffer-move-20110305.249/buffer-move-pkg.el
@@ -0,0 +1,1 @@
+(define-package "buffer-move" "20110305.249" "Swap buffers without typing C-x b on each window [wiki]" (quote nil))
