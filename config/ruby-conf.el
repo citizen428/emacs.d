e107b094c1acae648741743947725b6c6c7201d0
Michael Kohl <citizen428@gmail.com>
2014-02-06 12:23:01 +0700

    Directly incorporate bits of starter-kit-ruby

--- config/ruby-conf.el
+++ config/ruby-conf.el
@@ -28,11 +28,54 @@
 (add-hook 'ruby-mode-hook 'rinari-minor-mode)
 (add-hook 'ruby-mode-hook 'whitespace-mode)
 
-;; Use Pry as inferior Ruby implementation
-(add-to-list 'inf-ruby-implementations '("pry" . "pry -f"))
-(setq inf-ruby-default-implementation "pry")
-(setq inf-ruby-first-prompt-pattern "^\\[[0-9]+\\] pry\\((.*)\\)> *")
-(setq inf-ruby-prompt-pattern "^\\[[0-9]+\\] pry\\((.*)\\)[>*\"'] *")
+;; Stuff taken from starter-kit-ruby
+;; https://github.com/eschulte/emacs24-starter-kit/blob/master/starter-kit-ruby.org
+(eval-after-load 'ruby-mode
+  '(progn
+     ;; work around possible elpa bug
+     (ignore-errors (require 'ruby-compilation))
+     (setq ruby-use-encoding-map nil)
+     (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
+     (define-key ruby-mode-map (kbd "C-M-h") 'backward-kill-word)
+     (define-key ruby-mode-map (kbd "C-c l") "lambda")))
+
+(global-set-key (kbd "C-h r") 'ri)
+
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+
+(add-to-list 'completion-ignored-extensions ".rbc")
+
+(defun pcomplete/rake ()
+  "Completion rules for the `ssh' command."
+  (pcomplete-here (pcmpl-rake-tasks)))
+
+(defun pcmpl-rake-tasks ()
+  "Return a list of all the rake tasks defined in the current projects."
+  (delq nil (mapcar (lambda (line)
+                      (if (string-match "rake \\([^ ]+\\)" line) (match-string 1 line)))
+                    (split-string (shell-command-to-string "rake -T") "[\n]"))))
+
+(defun rake (task)
+  "Run rake task TASK."
+  (interactive (list (completing-read "Rake (default: default): "
+                                      (pcmpl-rake-tasks))))
+  (shell-command-to-string (concat "rake " (if (= 0 (length task)) "default" task))))
+
+                                        ; Clear the compilation buffer between test runs.
+(eval-after-load 'ruby-compilation
+  '(progn
+     (defadvice ruby-do-run-w/compilation (before kill-buffer (name cmdlist))
+       (let ((comp-buffer-name (format "*%s*" name)))
+         (when (get-buffer comp-buffer-name)
+           (with-current-buffer comp-buffer-name
+             (delete-region (point-min) (point-max))))))
+     (ad-activate 'ruby-do-run-w/compilation)))
+
+(setq rinari-major-modes
+      '(dired-mode 'ruby-mode 'css-mode 'yaml-mode 'javascript-mode))
 
 (provide 'ruby-conf)
+
 ;;; ruby-conf.el ends here
